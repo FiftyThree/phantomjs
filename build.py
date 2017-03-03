a9a5b8d5d2967b7e897c7fde206d97dd529b7e8b
Eric Nichols <ericn@fiftythree.com>
2017-03-03 12:30:29 -0700

    remove another config opt

--- build.py
+++ build.py
@@ -208,7 +208,6 @@ class PhantomJSBuilder(object):
                 "-no-mtdev",
                 "-no-libudev",
                 "-no-evdev",
-                "-no-pulseaudio",
                 "-no-alsa",
                 "-no-feature-PRINTPREVIEWWIDGET"
             ]
